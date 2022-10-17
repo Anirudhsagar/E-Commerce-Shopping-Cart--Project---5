03cfd010e69b238bf1d5816432484850ac690dbb
Upendra731996 <upendragupta731996@gmail.com>
2022-10-17 16:43:56 +0530

    sdfgh

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,3 +1,95 @@
+
+const productModel  = require('../models/productModel');
+const userModel = require('../models/userModel');
+const cartModel = require('../models/cartModel');
+const { isValidObjectId } = require('mongoose');
+
+// =================== Create Cart Api ========================
+const CreateCart = async (req,res)=> {
+    try {
+        let userId = req.params.userId
+        if(!isValidObjectId(userId))
+          { return res.status(404).send({ status: false, message: "Invalid UserId" }) }
+
+        //   ========== DB Call For  Checking UserId from UserModel===========
+
+        let checkId = await userModel.findById(userId)
+        if(!checkId)
+        return res.status(200).send({ status: true, msg: "Not ok" })
+
+        let cart = req.body
+        let { productId } = cart
+        if (Object.keys(cart).length < 1) { return res.status(400).send({ status: false, message: "create cart" }) }
+         if (!isValidObjectId(productId)) { return res.status(404).send({ status: false, message: "Invalid productId" }) }
+
+         
+        // -----------------------DB CALL FOR CHECKING PRODUCT ID FROM PRODUCT MODEL------------------
+        let productIdNew = await productModel.findOne({ _id: productId, isDeleted: false })
+        if (!productIdNew) {
+            return res.status(200).send({ status: true, message: "Not ok productId" })
+        }
+
+        // -------------------------------TO CREATE A NEW CART-----------------
+        let arr1 = []
+        let products = {
+            productId: productId,
+            quantity: 1
+        }
+        arr1.push(products)
+        let priceCalculated = productIdNew.price * products.quantity
+
+        // -------------------------------IF CART IS NOT CREATED OF A PARTICULAR USER IT WILL CREATE IT-----------------
+        let cartData = await cartModel.findOne({ userId: userId })
+        if (!cartData) {
+            let createCart = {
+                userId: userId, items: arr1,
+                totalPrice: priceCalculated, totalItems: 1
+            }
+            let saveData = await cartModel.create(createCart)
+            return res.status(201).send({ status: true, message: "newCart", data: saveData })
+        }
+
+        // -------------------------------WHEN CART IS ALREADY CREATED IT WILL UPDATE THE CART BY ITEMS(PRODUCT,QUANTITY,PRICE,ITEMS)------------------
+        if (cartData) {
+            let arr2 = cartData.items
+            let productAdded = {
+                productId: productId,
+                quantity: 1
+            }
+
+            // ------------------COMPARE THE PRODUCT ID IF NOT PRESENT IT WILL CREATE ONE IF PRESENT IT WILL UPDATE THE QUANTITY------------------
+            let compareProductId = arr2.findIndex((obj) => obj.productId == productId);
+            if (compareProductId == -1) {
+                arr2.push(productAdded)
+            } else {
+                arr2[compareProductId].quantity += 1;
+            }
+
+            let priceUpdated = cartData.totalPrice + (productIdNew.price * products.quantity)
+            let itemsUpdated = arr2.length
+
+            // -------------------------------CREATE THE UPDATED CART------------------
+            let createCartNew = {
+                items: arr2,
+                totalPrice: priceUpdated, totalItems: itemsUpdated
+            }
+            let saveData = await cartModel.findOneAndUpdate({ userId: userId }, createCartNew, { new: true })
+            return res.status(201).send({ status: true, message: "addCart", data: saveData })
+        }
+      
+    } catch (error) {
+      return res.status(500).send({ status: false, message: error.message })
+    }
+}
+
+
+
+
+
+
+
+
+
 const getCart =async function (req,res){
     try{
         let Id =req.params.userId
@@ -60,4 +152,4 @@ const deleteCart =async function (req,res){
     }
 }
 
-module.exports ={deleteCart,getCart}
+module.exports ={deleteCart,getCart,CreateCart }
---
+++ src/models/cartModel.js
@@ -0,0 +1,39 @@
+const { default: mongoose } = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+
+const cartSchema = new mongoose.Schema ({
+
+    userId: {
+        type: ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+    },
+
+    items: [{
+        productId: {
+            type: ObjectId,
+            ref: 'product',
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            min: 1
+        }
+    }],
+    totalPrice:{
+        type: Number,
+        required: true
+    },
+    totalItems: {
+        type: Number,
+        required: true,
+    },
+
+},{ timestamps: true})
+
+//  ----------------- 
+
+module.exports = mongoose.Model( 'Cart', cartSchema)
